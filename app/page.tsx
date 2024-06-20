5d2bc2a7a91d47969ecc5ba5e23e858e5a9c140c
Michal Shelenberg <email@michalshelenberg.com>
2024-06-20 13:48:57 +0200

    Add social badges

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
 import Projects from "@/components/projects";
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <main className="p-6 md:p-12 space-y-12">
-      <header className="py-16 max-w-[800px] w-full">
+      <header className="py-8 max-w-[800px] w-full">
         <h1 className="hidden">Michal Shelenberg</h1>
         <p className="md:text-3xl text-2xl">
-          Web developer, specializing in Next.js and React Native applications,
+          Web developer, specializing in Next.js, React Native applications,
           games, and robotics
         </p>
+        {/* Social badges */}
+        <div className="flex flex-row gap-3 mt-6">
+          <Link
+            href={"https://github.com/msxgg"}
+            target="_blank"
+            className={`flex flex-row py-1.5 text-white pr-3 pl-1.5 gap-1.5 rounded-full bg-black hover:scale-105 transition duration-300 items-center justify-center`}
+          >
+            {<FaGithub size={20} />}
+            {"GitHub"}
+          </Link>
+          <Link
+            href={"https://www.linkedin.com/in/msxgg/"}
+            target="_blank"
+            className={`flex flex-row py-1.5 text-white pr-3 pl-3 gap-1.5 rounded-full bg-[#0A66C2] hover:scale-105 transition duration-300 items-center justify-center`}
+          >
+            {<FaLinkedinIn size={20} />}
+            {"LinkedIn"}
+          </Link>
+        </div>
       </header>
       <Projects />
     </main>
